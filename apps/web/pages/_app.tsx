9750494514d7b9400be4072a6e6aecd866b5a8a3
Zobeir Hamid <zobeirhamid@berkeley.edu>
2024-05-22 17:48:31 -0700

    feat: adds deployment

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: any) {
   return (
     <MantineProvider theme={theme}>
       <Head>
-        <title>Mantine Template</title>
+        <title>Trieve - OpenVC</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
---
+++ release.sh
@@ -0,0 +1,3 @@
+docker-compose -f docker-compose.release.yml build
+
+docker push zobeirhamid0708/trieve-openvc:latest
